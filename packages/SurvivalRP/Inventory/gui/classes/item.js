415e3e10712027c9ffe044227a3be96b97a054c9
Eithing <eithing62@gmail.com>
2019-12-25 23:28:14 +0100

    refacto Project gesture

---
+++ packages/SurvivalRP/Inventory/gui/classes/item.js
@@ -0,0 +1,24 @@
+class Item {
+    constructor(type, imageId, ammout) {
+        //info sur l'item
+        this.element = newEl('div');
+        this.element.id = type.toString();
+
+        this.element.classList.add('item');
+
+        this.image = newEl('img');
+        this.image.classList.add('item-image')
+        this.image.src = "./../../Ressources/" + type + "/" + imageId + ".jpg"
+        this.element.appendChild(this.image);
+
+        this.label = newEl('label');
+        if (ammout > 999) {
+            ammout = "999+"
+        }
+        this.label.innerHTML = ammout.toString(); //set ammout in label
+        this.label.classList.add('item-label')
+        this.element.appendChild(this.label);
+
+        return this;
+    }
+}
