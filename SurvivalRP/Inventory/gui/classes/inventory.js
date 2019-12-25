288bb66d0e032223f5d0cd72d2e4cdeea9b7f810
Eithing <eithing62@gmail.com>
2019-12-25 03:23:39 +0100

    add selecteur de type dans l'inventaire + surbrillance des cases ok

--- SurvivalRP/Inventory/gui/classes/inventory.js
+++ SurvivalRP/Inventory/gui/classes/inventory.js
@@ -36,51 +36,87 @@ class Inventory {
         this.dragging = null;
         this.elementToDrag = null;
         this.dropIntoTarget = null;
-        let previousContainer;
 
+        let previousContainer;
+        let previousImageDiv
         this.mouseDownHandler = (evt) => {
             evt.preventDefault();
             //on recupere la cible cliquer
             let target = evt.target.parentNode;
             previousContainer = evt.target.parentNode.parentNode;
+            previousImageDiv = evt.target.parentNode;
             if (target && target.className === 'item' && this.dragging == null) {
                 this.elementToDrag = target;
                 this.elementToDrag.style.opacity = 0.5;
                 this.dragging = target.cloneNode(true);
                 this.dragging.classList.add('item-draggable');
                 this.element.appendChild(this.dragging);
+
+                //surbrillance des case cibles
+                if(previousImageDiv.id == "weapons")
+                {
+                    let elements = document.getElementsByClassName("inventory-slot");
+                    for (var i = 0; i < elements.length; i++) {
+                        elements[i].style.backgroundColor="rgba(75, 192, 94, 0.25)";
+                    }
+                }
+                if(previousImageDiv.id == "helmet")
+                {
+                    document.getElementById("player-inventory-stuff1").style.backgroundColor="rgba(75, 192, 94, 0.25)";
+                }
+                if(previousImageDiv.id == "jacket")
+                {
+                    document.getElementById("player-inventory-stuff2").style.backgroundColor="rgba(75, 192, 94, 0.25)";
+                }
+                if(previousImageDiv.id == "pant")
+                {
+                    document.getElementById("player-inventory-stuff3").style.backgroundColor="rgba(75, 192, 94, 0.25)";
+                }
+                if(previousImageDiv.id == "shoes")
+                {
+                    document.getElementById("player-inventory-stuff4").style.backgroundColor="rgba(75, 192, 94, 0.25)";
+                }
             }
         }
         this.mouseUpHandler = (evt) => {
             evt.preventDefault();
+
+            let elements = document.getElementsByClassName("inventory-slot");
+            for (var i = 0; i < elements.length; i++) {
+                elements[i].style.backgroundColor= "rgba(240, 255, 240, 0.11)";
+            }
+            document.getElementById("player-inventory-stuff1").style.backgroundColor="rgba(240, 255, 240, 0.11)";
+            document.getElementById("player-inventory-stuff2").style.backgroundColor="rgba(240, 255, 240, 0.11)";
+            document.getElementById("player-inventory-stuff3").style.backgroundColor="rgba(240, 255, 240, 0.11)";
+            document.getElementById("player-inventory-stuff4").style.backgroundColor="rgba(240, 255, 240, 0.11)";
 
             let target = evt.target.parentNode;
             if (target && target.className === 'item' && this.dragging !== null) {
                 this.dropIntoTarget = target;
             }
             target = evt.target;
-            if (target && target.id === 'player-inventory-stuff1' && this.dragging !== null && previousContainer.id != target.id) {
+            if (target && target.id === 'player-inventory-stuff1' && this.dragging !== null && previousContainer.id != target.id && previousImageDiv.id == "helmet") {
                 this.dropIntoTarget = target;
             }
-            if (target && target.id === 'player-inventory-stuff2' && this.dragging !== null && previousContainer.id != target.id) {
+            if (target && target.id === 'player-inventory-stuff2' && this.dragging !== null && previousContainer.id != target.id && previousImageDiv.id == "jacket") {
                 this.dropIntoTarget = target;
             }
-            if (target && target.id === 'player-inventory-stuff3' && this.dragging !== null && previousContainer.id != target.id) {
+            if (target && target.id === 'player-inventory-stuff3' && this.dragging !== null && previousContainer.id != target.id && previousImageDiv.id == "pant") {
                 this.dropIntoTarget = target;
             }
-            if (target && target.id === 'player-inventory-stuff4' && this.dragging !== null && previousContainer.id != target.id) {
+            if (target && target.id === 'player-inventory-stuff4' && this.dragging !== null && previousContainer.id != target.id && previousImageDiv.id == "shoes") {
                 this.dropIntoTarget = target;
             }
             if (target && target.id === 'player-inventory-container' && this.dragging !== null && previousContainer.id != target.id) {
                 this.dropIntoTarget = target;
             }
-            if (target && target.id === 'player-inventory-slot1' && this.dragging !== null && previousContainer.id != target.id) {
+            if (target && target.id === 'player-inventory-slot1' && this.dragging !== null && previousContainer.id != target.id && previousImageDiv.id == "weapons") {
                 this.dropIntoTarget = target;
             }
-            if (target && target.id === 'player-inventory-slot2' && this.dragging !== null && previousContainer.id != target.id) {
+            if (target && target.id === 'player-inventory-slot2' && this.dragging !== null && previousContainer.id != target.id && previousImageDiv.id == "weapons") {
                 this.dropIntoTarget = target;
             }
-            if (target && target.id === 'player-inventory-slot3' && this.dragging !== null && previousContainer.id != target.id) {
+            if (target && target.id === 'player-inventory-slot3' && this.dragging !== null && previousContainer.id != target.id && previousImageDiv.id == "weapons") {
                 this.dropIntoTarget = target;
             }
         }
--- SurvivalRP/Inventory/gui/classes/item.js
+++ SurvivalRP/Inventory/gui/classes/item.js
@@ -1,5 +1,5 @@
 class Item {
-    constructor(type, imageId /* cloth | weapons | cars */ ) {
+    constructor(type, imageId) {
         //info sur l'item
         this.element = newEl('div');
         this.element.id = type.toString();
@@ -8,21 +8,7 @@ class Item {
 
         this.image = newEl('img');
         this.image.classList.add('item-image')
-            /* j'ai mis des images random, a toi de mettre les tiennes*/
         this.image.src = "./../../Ressources/" + type + "/" + imageId + ".jpg"
-            /* switch (type) {
-                case "cloth":
-                    this.image.src = "./../../Ressources/weapons/1.jpg";
-                    break;
-                case "weapons":
-                    this.image.src = "./../../Ressources/weapons/" + imageId + ".jpg";
-                    break;
-                case "cars":
-                    this.image.src = "./../../Ressources/weapons/3.jpg";
-                    break;
-                default:
-                    //this.image.src = "./../../Ressources/weapons/3.jpg";
-            } */
         this.element.appendChild(this.image);
         return this;
     }
